8e51d88e7fab4fe70488e1d68feae09409324688
Ziran Sun <ziran.sun@samsung.com>
2013-03-12 18:01:22 +0000

    HRM end to end demo

--- webinos/core/wrt/lib/webinos.discovery.js
+++ webinos/core/wrt/lib/webinos.discovery.js
@@ -73,7 +73,7 @@
 
 	DiscoveryModule.prototype.findHRM = function(data, success){
 		console.log("HRM find HRM");
-  		var rpc = webinos.rpcHandler.createRPC(this, "findHRM",data);
+  		var rpc = webinos.rpcHandler.createRPC(this, "findHRM", arguments);
 	  	webinos.rpcHandler.executeRPC(rpc, function(params) {
 	  		success(params);
 	  	});
